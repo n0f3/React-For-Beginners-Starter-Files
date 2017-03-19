1ce333c5c9204a15157ce0fdb98c9919432ea955
Alessandro Metta <Alessandro Metta>
2017-03-18 18:54:01 -0700

    Added states and showing fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,16 +3,53 @@ import StorePicker from './StorePicker';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    // need to bind the method to the object to make it available
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    // This is going to be our initial state for the class
+    this.state = {
+        fishes: {},
+        order: {}
+    };
+  }
+  addFish(fish) {
+    // update our state
+    // take a copy of our existing state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now(); // gets current time in milliseconds
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    // we are telling react that we just changed this single piece of the state
+    // without having to update the entire state.
+    this.setState({ fishes: fishes });
+  }
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+            }
+          </ul>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
   goToStore(event) {
     event.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
   }
   render() {
     return (
-        <form className="store-selector" onSubmit={this.goToStore}>
+        <form className="store-selector" onSubmit={(e) => {this.goToStore(e)}}/*this.goToStore.bind(this)}*/>
           { /*Hello this is how you comment in jsx. Don't put at top level or 
               it will try to return as a top level object.*/ }
           <h2>Please Enter A Store</h2>
@@ -18,4 +24,10 @@ class StorePicker extends React.Component {
       );
   }
 }
+
+// Tell react that the StorePicker component is expecting a router component
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+}
+
 export default StorePicker;
