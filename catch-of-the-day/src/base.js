b8376a33bd0115ef2f8997e6435ebcf9c1b32de3
Alessandro Metta <Alessandro Metta>
2017-03-19 19:58:21 -0700

    Added firebase state syncing

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBvpKszM8OvUlZIc1WX2vvMqs85pkIufHk",
+  authDomain: "catch-of-the-day-metta.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-metta.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import StorePicker from './StorePicker';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -12,11 +12,33 @@ class App extends React.Component {
     // need to bind the method to the object to make it available
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
     // This is going to be our initial state for the class
     this.state = {
         fishes: {},
         order: {}
     };
+  }
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+    //check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    if(localStorageRef) {
+      // Update our app component order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+      console.log(`${JSON.stringify(this.state)}`)
+    }
+  }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
   addFish(fish) {
     // update our state
@@ -35,6 +57,14 @@ class App extends React.Component {
       fishes: sampleFishes
     });
   }
+  addToOrder(key) {
+    // make a copy of our state
+    const order = {...this.state.order};
+    // Update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -44,11 +74,14 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
-        <Order/>
+        <Order 
+          fishes={this.state.fishes} 
+          order={this.state.order}
+          params={this.props.params}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
